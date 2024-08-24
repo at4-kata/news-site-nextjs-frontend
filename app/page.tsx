7b93c5e0aed6e4887916d94e86d15d13a646d40f
at4-kata <76214425+at4-kata@users.noreply.github.com>
2024-08-24 14:08:06 +0900

    finished 3-3-1

--- app/page.tsx
+++ app/page.tsx
@@ -43,6 +43,7 @@ const data: { contents: News[] } = {
 
 export default function Home() {
   const name: string = "world";
+  const sliceData = data.contents.slice(0, 2);
   return (
     <>
       <section className={styles.top}>
@@ -63,7 +64,7 @@ export default function Home() {
       <section className={styles.news}>
         <h2 className={styles.newsTitle}>News</h2>
         <ul>
-          {data.contents.map((article) => (
+          {sliceData.map((article) => (
             <li key={article.id} className={styles.list}>
               <div className={styles.link}>
                 <Image
